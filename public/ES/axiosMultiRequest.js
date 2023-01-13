01adb85ec53a081d3b3066d2fdf148f3b447b906
waldemar <vova108vova@gmail.com>
2023-01-13 23:39:01 +0300

    axios multirequest

---
+++ public/ES/axiosMultiRequest.js
@@ -0,0 +1,19 @@
+
+window.addEventListener("DOMContentLoaded", () => {
+const getAxios = (axios) => {
+    return axios.get("https://jsonplaceholder.typicode.com/users");
+};
+const userData = {
+    name: 'Waldemar',
+    age: 40,
+};
+const postAxios = (axios) => { 
+    return axios.post("https://jsonplaceholder.typicode.com/users", userData );
+};
+
+    Promise.all([getAxios(axios), postAxios(axios)])
+        .then((response) => {
+            console.log(response[0].data);
+            console.log(response[1].data);
+        });
+});
